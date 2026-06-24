abc855660cf65876c55ae2bc849f1e059b1fc6eb
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Glue/ProductLabelsRestApi/ProductLabelsRestApiConfig.php
+++ src/Spryker/Glue/ProductLabelsRestApi/ProductLabelsRestApiConfig.php
@@ -12,36 +12,50 @@ use Spryker\Glue\Kernel\AbstractBundleConfig;
 class ProductLabelsRestApiConfig extends AbstractBundleConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_PRODUCT_LABELS = 'product-labels';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTROLLER_PRODUCT_LABELS = 'product-labels-resource';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ACTION_PRODUCT_LABELS_GET = 'get';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CANT_FIND_PRODUCT_LABEL = '1201';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_CANT_FIND_PRODUCT_LABEL = 'Product label is not found.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_PRODUCT_LABEL_ID_IS_MISSING = '1202';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_PRODUCT_LABEL_ID_IS_MISSING = 'Product label id is missing.';
